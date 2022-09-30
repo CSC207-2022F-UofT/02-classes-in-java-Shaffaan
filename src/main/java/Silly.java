25c6959320d65f6b2589be10c203e99b5dbe5467
= <=>
2022-09-30 00:04:23 -0400

    work

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -59,6 +59,7 @@ public class Silly implements Comparable<Silly>{
      * @param name the name of this Silly instance.
      */
     public Silly(String name) {
+
         this.name = name;
     }
 
@@ -79,6 +80,17 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
+
+    /**
+     * Creates a new silly object
+     * This constructor takes in a single int as an argument
+     *
+     * sets this.name to the concatenation of both the strings
+     */
+
+    public Silly(String a, String b) {
+        this.name = a + b;
+    }
 
 
 
@@ -116,7 +128,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = new int[]{0,1,2,3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +146,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -158,6 +171,8 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+
+        return this.name.equals(other.name);
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
@@ -194,6 +209,16 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+
+        if (this.name.length() == other.name.length() ) {
+            return 0;
+        }
+        else if (this.name.length() > other.name.length()) {
+            return 5;
+        }
+        else {
+            return -3;
+        }
     }
 
     /*
